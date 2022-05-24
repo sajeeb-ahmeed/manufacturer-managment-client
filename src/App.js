c11763f47579b06dab7f7e32173f5b2dc9e1cd10
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-05-24 17:02:49 +0600

    navbar banner added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Pages/Navbar';
+import { Route, Routes } from 'react-router';
+import Home from './Pages/Home/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <p>hello</p>
+    <div className=''>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Banner from './Banner';
+
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <div className='max-w-7xl mx-auto px-12'>
+            </div>
+        </div>
+
+
+    );
+};
+
+export default Home;
