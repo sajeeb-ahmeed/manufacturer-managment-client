c1f7593c42bc173b21f1379f52ddb2a4f06ef3fc
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-05-25 21:19:37 +0600

    banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Banner.css"
+import { Link } from 'react-router-dom';
+import plumber from '../../img/plumbing.png'
+const Banner = () => {
+  return (
+    <div className="container-fluid">
+      <div className="cursor">
+        <Link to='/pruches'>
+          <img src={plumber} className=" img-fluid rounded-lg shadow-2xl" alt='' />
+        </Link>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Banner;
