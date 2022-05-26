d324c0d81de979c983d805841554d8f678bda206
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-05-26 21:11:34 +0600

    done

--- src/components/firebase.init.js
+++ src/components/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyChiqJu3ytsC3kQqCYYvWJxeCsZh7SvcK4",
-  authDomain: "manufacture-hardware.firebaseapp.com",
-  projectId: "manufacture-hardware",
-  storageBucket: "manufacture-hardware.appspot.com",
-  messagingSenderId: "992203585624",
-  appId: "1:992203585624:web:1ae50f624ebe695ff5b22d"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
